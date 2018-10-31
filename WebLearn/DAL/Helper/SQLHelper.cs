92f197ccec2a78a3bbe7ffcb099194d822aa1d98
DiorSao <839684446@qq.com>
2018-10-31 19:32:26 +0800

    update

--- WebLearn/DAL/Helper/SQLHelper.cs
+++ WebLearn/DAL/Helper/SQLHelper.cs
@@ -5,10 +5,81 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
-
+using System.Configuration;
 namespace DAL
 {
+    /// <summary>
+    /// 通用数据访问类
+    /// </summary>
     class SQLHelper
     {
+        private static readonly string connString = ConfigurationManager.ConnectionStrings["connString"].ToString();
+        /// <summary>
+        /// 对数据进行赠删改
+        /// </summary>
+        /// <param name="sql">T-SQL语句</param>
+        /// <returns></returns>
+        public static int Update(string sql)
+        {
+            int result = 0;
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand(sql,conn);
+            try
+            {
+                conn.Open();
+                result = cmd.ExecuteNonQuery();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        /// <summary>
+        /// 得到单一结果
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static object GetSingleResult(string sql)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            try
+            {
+                conn.Open();
+                return cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        /// <summary>
+        /// 得到结果集
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static SqlDataReader GetReader(string sql)
+        {
+            SqlConnection conn = new SqlConnection();
+            SqlCommand cmd = new SqlCommand(sql,conn);
+            try
+            {
+                return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                throw;
+            }
+        }
     }
 }
--- WebLearn/Models/SysAdmin.cs
+++ WebLearn/Models/SysAdmin.cs
@@ -8,7 +8,18 @@ using System.Text;
 
 namespace Models
 {
-    class SysAdmin
+    /// <summary>
+    /// 如果你做远程方法调用(RPC）时，比如，服务器端有个类A及对象a，客户端需要无视网络的存在，直接调用对象a。
+    /// 这种情况下，就需要把类A设计为可序列化的，那么它的实例a也就可以实例化了。
+    /// 说得简单点，在服务器的 命名空间中的对象a, 肯定不能直接被客户端的命名空间中的一个对象调用，这不可能，对吧。
+    /// 但是如果你把服务器的对象a中的特征属性（不一定是所有属性）变成xml，传送到客户端，然后客户端用你给的特征属性可以模拟的生成一个对象a。
+    /// 这个过程就是 rpc，而要把对象a变成xml，就是序列化，反之，把xml变成模拟对象a就是反序列化。
+    /// </summary>
+    [Serializable]
+    public class SysAdmin
     {
+        public int LoginId { set; get; }
+        public string AdminName { get; set; }
+        public string AdminPwd { get; set; }
     }
 }
